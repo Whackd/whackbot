9ef845875dd0c4250e5dc443beac6f8e0b69d080
snowkidind <keny@eastcoastbands.com>
2020-02-02 00:44:04 +0700

    hello

--- telegram.js
+++ telegram.js
@@ -4,23 +4,16 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const TelegramBot = require('node-telegram-bot-api');
-
-
 const telegram = new TelegramBot(process.env.TELEGRAM_API_KEY, {polling: true});
+
 
 module.exports = {
 
   init:function(){
     console.log("Hola")
-
-
-      // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
-      telegram = new
-
     telegram.on("text", (message) = > {
-      telegram.sendMessage(message.chat.id, "Hello world");
-  })
-    ;
+      telegram.sendMessage(message.chat.id, "Sup.");
+    });
 
   }
 }
